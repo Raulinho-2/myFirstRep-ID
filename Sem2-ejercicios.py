2a4a8db8074451652783732b9e9f697478a066b6
Raulinho-2 <143754560+Raulinho-2@users.noreply.github.com>
2023-09-07 20:11:36 +0000

    py

---
+++ Sem2-ejercicios.py
@@ -0,0 +1,6 @@
+lista_cursos = ['Matemáticas', 'Física', 'Química', 'Historia', 'Lengua']
+def imprimir(lista):
+    print('Asignaturas:')
+    for i in lista:
+        print(i)
+imprimir(lista_cursos)
